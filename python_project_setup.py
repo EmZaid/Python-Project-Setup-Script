f859cc61bc5af66aaaf14afc3eae0e64b02e4225
EmZaid <98169817+EmZaid@users.noreply.github.com>
2022-04-22 10:21:21 -0400

    Update python_project_setup.py
    Added command line arguments. Will update readme shortly.

--- python_project_setup.py
+++ python_project_setup.py
@@ -2,29 +2,74 @@
 import os
 import subprocess
 import sys
+import argparse
+from pathlib import Path
 
 # Change this to where you python projects live
-if __name__ == "__main__":
-    PROJECT_PATH = "~/"
 
-    # The program will ask you what your project name will be
+
+# PROJECT_PATH = "~/"
+
+
+# Refactoring to use argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-a",
+    "-all",
+    dest="all",
+    help="Runs full script, minimal interaction possible depending on argumenst passed.",
+    action="store_true",
+)
+parser.add_argument(
+    "-n", "-name", dest="name", type=str, help="Input your project name"
+)
+
+
+parser.add_argument(
+    "--path",
+    type=lambda p: Path(p).absolute(),
+    default=Path(__file__).absolute().parent,
+    help="Path to the python project directory",
+)
+
+args = parser.parse_args()
+# Checks to make sure a project name was provided. If not prompts user for one.
+if not args.name:
     project_name = input("Project Name:")
+elif args.name != "":
+    project_name = args.name
 
-    new_proj_path = PROJECT_PATH + project_name + "/"
 
-    def create_folder():
-        """This will create a new project in Python Projects"""
+PROJ_DIR = str(args.path)
+new_proj_path = PROJ_DIR + "/" + project_name + "/"
+print(new_proj_path)
 
-        try:
-            os.mkdir(PROJECT_PATH + project_name)
-        except OSError:
-            print(f"(Creation of the directory {project_name} failed)")
-        else:
-            print(f"Successfully created the directory {project_name}")
 
-    def pyenv_options():
-        """This gives the user options about creating a virutalenv"""
+def create_folder():
+    """This will create a new project in Python Projects"""
 
+    try:
+        os.mkdir(new_proj_path)
+    except OSError:
+        print(
+            f"Creation of the directory {project_name} failed. Directory probably alreadly exists. Exiting"
+        )
+        sys.exit()
+    else:
+        print(f"Successfully created the directory {project_name}")
+
+
+if not args.all:
+    print("fuck this didn't work")
+elif args.all:
+    print(args.all)
+
+
+def pyenv_options():
+    """This gives the user options about creating a virutalenv"""
+
+    if not args.all:
         accepted_answ = ["y", "n", "exit", "yes", "no"]
         yes_answ = ["y", "yes"]
         no_answ = ["n", "no"]
@@ -46,9 +91,14 @@ if __name__ == "__main__":
         if answ == "exit":
             print("Exiting!")
             sys.exit(0)
+    elif args.all:
+        subprocess.run(["pyenv", "virtualenv", project_name], check=True)
+        return
 
-    def pyenv_local_options():
-        """lets you create a .python-version file"""
+
+def pyenv_local_options():
+    """lets you create a .python-version file"""
+    if not args.all:
         accepted_answ = ["y", "n", "exit", "yes", "no"]
         yes_answ = ["y", "yes"]
         no_answ = ["n", "no"]
@@ -62,9 +112,7 @@ if __name__ == "__main__":
             answ = answ.lower()
         while answ in yes_answ:
             print("Ok. Creating .python-version now:")
-            os.system(
-                "echo " + project_name + ">>" + new_proj_path + "/" + ".python-version"
-            )
+            os.system("echo " + project_name + ">>" + new_proj_path + ".python-version")
             return
         while answ in no_answ:
             print("Ok. Continuting on")
@@ -72,9 +120,14 @@ if __name__ == "__main__":
         if answ == "exit":
             print("Exiting!")
             sys.exit()
+    elif args.all:
+        os.system("echo " + project_name + ">>" + new_proj_path + ".python-version")
 
-    def pip_install():
-        """installs black and pylint and a .pylintrc file"""
+
+def pip_install():
+    """installs black and pylint and a .pylintrc file"""
+    if not args.all:
+
         accepted_answ = ["y", "n", "exit", "yes", "no"]
         yes_answ = ["y", "yes"]
         no_answ = ["n", "no"]
@@ -111,6 +164,18 @@ if __name__ == "__main__":
             print("Exiting!")
             sys.exit()
 
+    elif args.all:
+        os.system(
+            "/Users/Emilienne/.pyenv/versions/"
+            + project_name
+            + "/bin/python -m pip install black pylint"
+        )
+        os.chdir(new_proj_path)
+        with open(".pylintrc", "w", encoding="UTF-8") as outfile:
+            subprocess.run(["pylint", "--generate-rcfile"], check=True, stdout=outfile)
+
+
+if __name__ == "__main__":
     create_folder()
     pyenv_options()
     pyenv_local_options()
